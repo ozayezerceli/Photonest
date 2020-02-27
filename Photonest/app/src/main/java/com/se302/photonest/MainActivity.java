3237a22c698746f1d4353d089eb96337c863e962
Özay Ezerceli <ozay_ezerceli804@hotmail.com>
2020-02-27 13:39:58 +0300

    Firebase connected

--- Photonest/app/src/main/java/com/se302/photonest/MainActivity.java
+++ Photonest/app/src/main/java/com/se302/photonest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.se302.photonest;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signInClicked(View view){
+
+    }
+
+    public void signUpClicked(View view){
+
     }
 }
