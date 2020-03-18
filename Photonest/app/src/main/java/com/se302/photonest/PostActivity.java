6b4e9201a33590dc577bc51e0fce735a8461b4c7
Dilara <dilara.unbay@isik.edu.tr>
2020-03-18 21:06:05 +0300

    Login Page Re-designed
    *Login page is re-designed.
    *Background color added to all pages.

--- Photonest/app/src/main/java/com/se302/photonest/PostActivity.java
+++ Photonest/app/src/main/java/com/se302/photonest/PostActivity.java
@@ -1,15 +1,10 @@
 package com.se302.photonest;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
-
-import Utils.BottomNavigationViewHelper;
 
 public class PostActivity extends AppCompatActivity {
 
