58461bb61f66ca8ae7859ce2fb7099250ac719a9
Dilara <dilara.unbay@isik.edu.tr>
2020-03-18 20:05:45 +0300

    Navigation Bar updated
    Navigation bar is added to all related activities and xml files.

--- Photonest/app/src/main/java/Utils/BottomNavigationViewHelper.java
+++ Photonest/app/src/main/java/Utils/BottomNavigationViewHelper.java
@@ -3,7 +3,6 @@ package Utils;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
@@ -19,10 +18,8 @@ import com.se302.photonest.SearchActivity;
 
 public class BottomNavigationViewHelper {
 
-    private static final String TAG = "BottomNavigationViewHel";
 
     public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
-        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
         bottomNavigationViewEx.enableAnimation(false);
         bottomNavigationViewEx.enableItemShiftingMode(false);
         bottomNavigationViewEx.enableShiftingMode(false);
@@ -34,33 +31,33 @@ public class BottomNavigationViewHelper {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
-
+                    //Goes to feed. ACTIVITY_NUM = 0
                     case R.id.ic_feed:
-                        Intent intent1 = new Intent(context, MainActivity.class);//ACTIVITY_NUM = 0
+                        Intent intent1 = new Intent(context, MainActivity.class);
                         context.startActivity(intent1);
                         callingActivity.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                         break;
-
+                    //Goes to search. ACTIVITY_NUM = 1
                     case R.id.ic_search:
-                        Intent intent2  = new Intent(context, SearchActivity.class);//ACTIVITY_NUM = 1
+                        Intent intent2  = new Intent(context, SearchActivity.class);
                         context.startActivity(intent2);
                         callingActivity.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                         break;
-
+                    //Goes to add post. ACTIVITY_NUM = 2. Nav bar should disappear.
                     case R.id.ic_addPost:
-                        Intent intent3 = new Intent(context, PostActivity.class);//ACTIVITY_NUM = 2
+                        Intent intent3 = new Intent(context, PostActivity.class);
                         context.startActivity(intent3);
                         callingActivity.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                         break;
-
+                    //Goes to notifications. ACTIVITY_NUM = 3
                     case R.id.ic_notification:
-                        Intent intent4 = new Intent(context, NotificationActivity.class);//ACTIVITY_NUM = 3
+                        Intent intent4 = new Intent(context, NotificationActivity.class);
                         context.startActivity(intent4);
                         callingActivity.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                         break;
-
+                    //Goes to profile. ACTIVITY_NUM = 4
                     case R.id.ic_profile:
-                        Intent intent5 = new Intent(context, ProfileActivity.class);//ACTIVITY_NUM = 4
+                        Intent intent5 = new Intent(context, ProfileActivity.class);
                         context.startActivity(intent5);
                         callingActivity.overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                         break;
--- Photonest/app/src/main/java/com/se302/photonest/NotificationActivity.java
+++ Photonest/app/src/main/java/com/se302/photonest/NotificationActivity.java
@@ -1,14 +1,35 @@
 package com.se302.photonest;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import Utils.BottomNavigationViewHelper;
+
 public class NotificationActivity extends AppCompatActivity {
+
+    private static final int ACTIVITY_NUM = 3;
+
+    private Context myContext = NotificationActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        setupBottomNavBar();
+    }
+
+    private void setupBottomNavBar(){
+        BottomNavigationViewEx bottomNavBar = (BottomNavigationViewEx) findViewById(R.id.bottomNavBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavBar);
+        BottomNavigationViewHelper.enableNavigation(myContext, this, bottomNavBar);
+        Menu menu = bottomNavBar.getMenu();
+        MenuItem mItem = menu.getItem(ACTIVITY_NUM);
+        mItem.setChecked(true);
     }
 }
