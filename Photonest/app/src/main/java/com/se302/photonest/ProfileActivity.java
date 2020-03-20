23564d09a1008c8dc037b3eae39421bf8c1a9ae5
Dilara <dilara.unbay@isik.edu.tr>
2020-03-20 04:40:38 +0300

    Profile Page updated
    *Profile menu is added (Logout transfered here)
    *Getting and displaying username. (Not working)
    *Top bar design is made.

--- Photonest/app/src/main/java/com/se302/photonest/ProfileActivity.java
+++ Photonest/app/src/main/java/com/se302/photonest/ProfileActivity.java
@@ -5,20 +5,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActionMenuView;
 
+import com.google.firebase.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import Utils.BottomNavigationViewHelper;
 
 public class ProfileActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    FirebaseDatabase firebaseDatabase;
+    private TextView username;
     private static final int ACTIVITY_NUM = 4;
-    Button logoutButton;
     private Context myContext = ProfileActivity.this;
 
 
@@ -26,18 +35,45 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        logoutButton = findViewById(R.id.profile_logout_button);
-        setupBottomNavBar();
+
+        username = findViewById(R.id.usernameTxt);
         mAuth = FirebaseAuth.getInstance();
-        logoutButton.setOnClickListener(new View.OnClickListener() {
+        //not working for now. Gets null.
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference rootref = database.getReference().child("root");
+        DatabaseReference userref = rootref.child("username");
+        userref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-                finish();
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                    String usern = (String) dataSnapshot.getValue();
             }
-        });
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+            });
+
+        ActionMenuView actionMenuView = findViewById(R.id.profile_menu_view);
+        Menu bottomMenu = actionMenuView.getMenu();
+        getMenuInflater().inflate(R.menu.profile_menu, bottomMenu);
+        setupBottomNavBar();
+
+        for (int i = 0; i < bottomMenu.size(); i++){
+            bottomMenu.getItem(i).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.profile_logout:
+                            mAuth.signOut();
+                            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                            break;
+                    }
+                    return false;
+                }
+            });
+        }
     }
 
     private void setupBottomNavBar(){
