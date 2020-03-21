1d327b731957f28f79459d220050d4be87c9fcf1
Özay Ezerceli <ozay_ezerceli804@hotmail.com>
2020-03-21 17:40:49 +0300

    Change Password updated
    current password part is added to change password function

--- Photonest/app/src/main/java/com/se302/photonest/ChangePasswordActivity.java
+++ Photonest/app/src/main/java/com/se302/photonest/ChangePasswordActivity.java
@@ -13,12 +13,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ChangePasswordActivity extends AppCompatActivity {
-    private EditText password;
-    private EditText confirmPassword;
+    private EditText password, confirmPassword, currentPassword;
     private Button Done;
     FirebaseAuth auth;
 
@@ -38,33 +39,42 @@ public class ChangePasswordActivity extends AppCompatActivity {
     private void init(){
         password = findViewById(R.id.txt_ChangePwd);
         confirmPassword = findViewById(R.id.txt_ChangePwd2);
+        currentPassword = findViewById(R.id.txt_CurrentPwd);
         Done = findViewById(R.id.btnChangePwd);
         auth = FirebaseAuth.getInstance();
     }
 
     private void changePwd(){
-        FirebaseUser user = auth.getCurrentUser();
+        final FirebaseUser user = auth.getCurrentUser();
         if(user != null) {
-            String password1 = password.getText().toString();
+            String currentPwd = currentPassword.getText().toString();
+            AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), currentPwd);
+            final String password1 = password.getText().toString();
             String confirmPassword1 = confirmPassword.getText().toString();
-            if (TextUtils.isEmpty(password1) || TextUtils.isEmpty(confirmPassword1)) {
-                Toast.makeText(this, "Password or ConfirmPassword can not be empty!", Toast.LENGTH_LONG).show();
+            if (TextUtils.isEmpty(password1) || TextUtils.isEmpty(confirmPassword1) || TextUtils.isEmpty(currentPwd)) {
+                Toast.makeText(this, "Current Password or Password or ConfirmPassword can not be empty!", Toast.LENGTH_LONG).show();
             } else if (!password1.equals(confirmPassword1)) {
                 Toast.makeText(this, "Password and ConfirmPassword must be the same!", Toast.LENGTH_LONG).show();
             } else {
-                user.updatePassword(password1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(ChangePasswordActivity.this, "Your password has been changed.", Toast.LENGTH_LONG).show();
-                            Intent intent = new Intent(ChangePasswordActivity.this, ProfileActivity.class);
-                            startActivity(intent);
-                            finish();
-                        } else {
-                            Toast.makeText(ChangePasswordActivity.this, "Error!", Toast.LENGTH_LONG).show();
-                        }
+                        user.updatePassword(password1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ChangePasswordActivity.this, "Your password has been changed.", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(ChangePasswordActivity.this, ProfileActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(ChangePasswordActivity.this, "Error!", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
                     }
                 });
+
             }
         }else{
             Toast.makeText(ChangePasswordActivity.this, "A user must be logged in!", Toast.LENGTH_LONG).show();
